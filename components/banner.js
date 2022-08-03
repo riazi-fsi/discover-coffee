f2fc17a5cacdcfa368d504c7c33fd1b66c63216b
F_Riazi <f_riazi@isc.co.ir>
2022-08-03 09:36:09 +0430

    add image

--- components/banner.js
+++ components/banner.js
@@ -1,9 +1,9 @@
 const Banner = (props) => {
     return (
         <div>
-            <h1><span>Coffee</span> Connoisseur</h1>
-            <p>Discover your local coffeeshop</p>
-            <button onClick={props.handleOnclick}>{props.buttonText}</button>
+            <h1 className="text-blue-700 font-bold text-5xl"><span className="text-white text-5xl block ">Coffee</span> Connoisseur</h1>
+            <p className="text-black text-2xl mt-3">Discover your local coffeeshop</p>
+            <button className="text-white bg-purple-600 p-3 px-4 mt-3 hover:border " onClick={props.handleOnclick}>{props.buttonText}</button>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Banner from '../components/banner'
 import styles from '../styles/Home.module.css'
+import Image from 'next/image'
 
 export default function Home() {
  const handleOnBannerBtnClick=()=>{
@@ -17,6 +18,10 @@ console.log('hi btn')
       <main className={styles.main}>
        
         <Banner buttonText='View stores nearby' handleOnclick={handleOnBannerBtnClick} />
+        <div className='absolute top-4.5 right-4'>
+        <Image  src="/static/coffee.png" width={400} height={200}/>
+        </div>
+       
       </main>
 
     </div>
