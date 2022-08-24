40687d49457c256fb44f2c70ba48ff5a4286001b
F_Riazi <f_riazi@isc.co.ir>
2022-08-24 11:52:01 +0430

    add card component

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import Head from 'next/head'
 import Banner from '../components/banner'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import Card from '../components/card'
 
 export default function Home() {
- const handleOnBannerBtnClick=()=>{
-console.log('hi btn')
+  const handleOnBannerBtnClick = () => {
+    console.log('hi btn')
   }
   return (
     <div className={styles.container}>
@@ -16,12 +17,17 @@ console.log('hi btn')
       </Head>
 
       <main className={styles.main}>
-       
+
         <Banner buttonText='View stores nearby' handleOnclick={handleOnBannerBtnClick} />
         <div className='absolute top-4.5 right-4'>
-        <Image  src="/static/coffee.png" width={400} height={200}/>
+          <Image src="/static/coffee.png" width={400} height={200} />
         </div>
-       
+        <div className='flex items-center flex-row mt-20 justify-between w-full'>
+          <Card name='dark coffee ' imgUrl={"/static/coffee.png"} href="/coffee-store/dark-coffee" />
+          <Card name='dark coffee ' imgUrl={"/static/coffee.png"} href="/coffee-store/dark-coffee" />
+          <Card name='dark coffee ' imgUrl={"/static/coffee.png"} href="/coffee-store/dark-coffee" />
+        </div>
+
       </main>
 
     </div>
